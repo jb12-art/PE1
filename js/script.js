9930324d348d9198800a34c83274ec6874e9b04f
jb12-art <jbqw1@msn.com>
2025-08-19 15:35:58 +0300

    files

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// PE1 - API Endpoint Online Shop.
+// Connected to Home page.
+// script.js is styled in product.css
---
+++ product/product-detail.js
@@ -0,0 +1,2 @@
+// PE1 - API Endpoint Online Shop.
+// product.js is styled in product-detail.css
